bdfb5abdfcd7a7b16638ae0aabb9d2dac7939237
bogdan naydenov <bnaydenov@gmail.com>
2012-03-06 17:13:47 +0200

    CreateServer functionality

---
+++ RackSpaceCloudServersAPI/RackSpaceCloudServerImages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RackSpaceCloudServersAPI
+{
+    public class RackSpaceCloudServerImage
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string updated { get; set; }
+        public string created { get; set; }
+        public string status { get; set; }
+        public int? serverId { get; set; }
+        public int? progress { get; set; }
+    }
+}
